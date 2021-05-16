50afbe3cc38afc59d7c5337d9e282c5c2a3e4d4f
B$T4 <77292192+beT4w@users.noreply.github.com>
2021-05-16 06:01:47 +0300

    Add files via upload

---
+++ Events/message.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js");
+const Settings = require('../Settings/Settings.json');
+let talkedRecently = new Set();
+
+module.exports = message => {
+  if (talkedRecently.has(message.author.id)) {
+    return;
+  }
+  talkedRecently.add(message.author.id);
+	setTimeout(() => {
+    talkedRecently.delete(message.author.id);
+  }, 0);
+  let client = message.client;
+  if (message.author.bot) return;
+  if (!message.content.startsWith(Settings.BotSettings.prefix)) return;
+  let command = message.content.split(' ')[0].slice(Settings.BotSettings.prefix.length);
+  let params = message.content.split(' ').slice(1);
+  let perms = client.elevation(message);
+  let cmd;
+  if (client.commands.has(command)) {
+    cmd = client.commands.get(command);
+  } else if (client.aliases.has(command)) {
+    cmd = client.commands.get(client.aliases.get(command));
+  }
+  if (cmd) {
+    if (perms < cmd.conf.permLevel) return;
+    cmd.run(client, message, params, perms);
+  }
+
+};
---
+++ README.md
@@ -0,0 +1,19 @@
+# Discord Register Bot
+
+# Kurulum
+* İlk olarak bilgisayarına [Node JS](https://nodejs.org/en/) indir.
+* Bu projeyi zip halinde indir.
+* Herhangi bir klasöre zipi çıkart.
+* Daha sonra  `settings` katagorisindeki `other.json` ve `Settings.json` dosyasının içindeki bilgileri doldur.
+* Sonra klasörün içerisinde bir `powershell` ya da `cmd` penceresi aç.
+* ```npm install``` yazarak tüm modülleri kur.
+* Kurulum bittikten sonra ```node beta.js``` yaz ve botu başlat.
+
+Tadaaa 🎉. Artık register botun hazır. Dilediğin gibi kullanabilirsin.
+
+# İletişim
+* [Discord Sunucum](https://discord.gg/58UAMVJTSH)
+* [Discord Profilim](https://discord.com/users/780891365063917572)
+* Herhangi bir hata bulmanız durumunda ya da yardım isteyeceğiniz zaman buralardan bana ulaşabilirsiniz.
+
+### NOT: Botta MIT lisansı bulunmaktadır. Bu botun dosyalarının benden habersiz paylaşılması/satılması durumunda gerekli işlemler yapılacaktır!
